7bb8e7418c3c35812bbfa92bd4a27edc2914c8f7
KoVaL0 <goks223300@gmail.com>
2020-01-29 12:25:58 +0300

    Adding React

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    // entry: './test.js'  //Точка входа, если требуется изменить исполняемый js файл
+    entry: "./src/index.js",  //Точка входа, если требуется изменить исполняемый js файл
+    output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+    },  
     devServer: {
         contentBase: './dist', //Директория расположения контента
         overlay: true //Для вывода ошибок в консоль
-    }
-}
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
